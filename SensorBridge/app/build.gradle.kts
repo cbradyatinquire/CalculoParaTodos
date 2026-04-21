176dc32bd0d4fc904a5af7b48e1fd50277f5c77b
Corey Brady <corey.brady@smu.edu>
2026-04-20 22:09:16 -0500

    bump version numbers to republish to testers

--- SensorBridge/app/build.gradle.kts
+++ SensorBridge/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "edu.smu.sensorbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3"
+        versionCode = 6
+        versionName = "0.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
